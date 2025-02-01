cacb2a723153bc0642658e5055b44b13435ffe82
tarrillollontopVictor <tarrillollontopvictormanuel04@gmail.com>
2025-02-01 10:35:45 -0500

    Microservicio de autenticacion

---
+++ auth-mcrs/src/main/java/com/viCode/auth_mcrs/AuthMcrsApplication.java
@@ -0,0 +1,13 @@
+package com.viCode.auth_mcrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMcrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMcrsApplication.class, args);
+	}
+
+}
